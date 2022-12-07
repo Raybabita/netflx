36acf51186ef5351d2a68867919062abf912c9ad
Raybabita <ray007oceans@gmail.com>
2022-12-08 00:04:20 +0530

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: true,
-  api_key: '285bb9715cde096a3fbb2cfdac23701f',
-  cognito: {
-    userPoolId: 'us-east-1_dKTHI6WEi',
-    userPoolWebClientId: 'hbbusigb3bonflejvr87sjufb'
-  },
-  AWS_ACCESS_KEY: "AKIAQBUPM7BVO6QCSTFF",
-  AWS_SECRET_KEY: "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"
-};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
